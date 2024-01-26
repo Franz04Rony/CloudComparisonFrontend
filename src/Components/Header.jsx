2be6e8a4038b008b2c31c2271ae65c072508af38
Franz04Rony <ventocilla2p@hotmail.com>
2024-01-25 23:28:46 -0500

    header details

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <section className="w-full flex justify-between items-center">
         <span></span>
         <h1 className="text-3xl lg:text-5xl font-bold ml-12 lg:ml-20">Cloud services comparison </h1>
-        <a href="https://github.com/Franz04Rony" target="_blank" className="text-teal-100 hover:text-teal-200" aria-label="View source on Github">
+        <a href="https://github.com/Franz04Rony/CloudComparisonFrontend" target="_blank" className="text-teal-100 hover:text-teal-200" aria-label="View source on Github">
             <Github width={100} height={100}/>
         </a>
     </section>
