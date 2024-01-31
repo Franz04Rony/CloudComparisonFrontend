8a8e1db18950f6afdbfb42cbe41a7420597cadcd
Franz04Rony <ventocilla2p@hotmail.com>
2024-01-31 05:48:08 -0500

    styles update. Now the only posible theme is dark

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,10 @@
 export const Card = ({title, link, provider, description}) => {
   return (
-    <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+    <div className="block max-w-sm p-6 border  rounded-lg shadow bg-gray-800 border-gray-700 hover:bg-gray-700">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{title}</h5>
       <a href={link} target="_blank" className="block">Link a la documentación </a>
       <span className="text-yellow-100">Provider: {provider}</span>
-      <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
+      <p className="font-normal text-gray-400">{description}</p>
     </div>
   )
 }
--- src/Components/SecondaryButton.jsx
+++ src/Components/SecondaryButton.jsx
@@ -1,11 +1,11 @@
 export const SecondaryButton = ({text, handlerClick = ()=>{}}) => {
   return (
     <button className="bg-white/5 
-            border dark:border-white/10 border-gray-400 rounded-full
+            border border-white/10  rounded-full
             flex justify-center items-center gap-x-2
             py-1 px-2 md:py-2 md:px-4
             text-xs md:text-base
-            text-black/70 dark:text-white/50
+            text-white/50
             transition
             hover:scale-110 hover:bg-white/10" 
             onClick={handlerClick}
