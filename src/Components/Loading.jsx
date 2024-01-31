b46a1f86eb527e07a1d0809291a61d3f6f0f1036
Franz04Rony <ventocilla2p@hotmail.com>
2024-01-31 07:27:52 -0500

    loading component updated

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,7 +1,10 @@
 
 export const Loading = () => {
   return (
-    <div className="w-screen h-screen mx-auto flex items-center">
+    <div className="mx-auto flex items-center 
+    text-2xl w-20 h-screen
+    lg:text-4xl lg:w-40 
+    animate-ping">
         Loading...
     </div>
   )
