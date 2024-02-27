d1f41b1f34371df63d09e7ac975d7ccb4040accb
Franz04Rony <ventocilla2p@hotmail.com>
2024-02-27 15:21:36 -0500

    fixing header style

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,8 @@ import { Github } from "../icons/icons"
 export const Header = () => {
   return (
     <section className="w-full flex justify-between items-center">
-        <span></span>
-        <h1 className="text-3xl lg:text-5xl font-bold ml-12 lg:ml-20">Cloud services comparison </h1>
+        <span className="w-[100px]"></span>
+        <h1 className="text-3xl lg:text-5xl font-bold">Cloud services comparison </h1>
         <a href="https://github.com/Franz04Rony/CloudComparisonFrontend" target="_blank" className="text-teal-100 hover:text-teal-200" aria-label="View source on Github">
             <Github width={100} height={100}/>
         </a>
